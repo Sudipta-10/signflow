edbfa19f78615ce21598c8acad23c3dcee9d6e53
Sudipta Pratiher <sudiptapratiher@gmail.com>
2026-02-24 03:16:15 +0530

    fix 3

--- DocsSignatureAppBE/src/main/java/com/example/DocsSignatureAppBE/DocsSignatureAppBeApplication.java
+++ DocsSignatureAppBE/src/main/java/com/example/DocsSignatureAppBE/DocsSignatureAppBeApplication.java
@@ -3,20 +3,11 @@ package com.example.DocsSignatureAppBE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
-@RestController
 public class DocsSignatureAppBeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocsSignatureAppBeApplication.class, args);
 	}
-
-    @GetMapping("/")
-    public String home() {
-        return "Verifile API is running successfully!";
-    }
 
 }
